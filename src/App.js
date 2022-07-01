afb4708c8b9beaff41b7162c3504e0b27f1cb36a
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2022-06-30 21:18:45 -0300

    bug: imprimindo estado anterior

--- src/App.js
+++ src/App.js
@@ -8,15 +8,20 @@ import "./App.css";
 function App() {
   const [list, setList] = useState("");
   const [term, setTerm] = useState("");
-  const fetchData = async()=>{
+  const [btn, setbtn] = useState(false);
+  
+  useEffect(()=>{
+    const fetchData = async()=>{
       const result = await api.get(`/search?query=${term}`);
       setList(result?.data?.hits);
-      console.log(list);
-  }
+    }
+    console.log(list)
+    fetchData();
+  },[btn])
   return (
     <>
       <SearchBar searchTerm={term} setSearchTerm={setTerm}/>
-      <SearchButton onButtonClick={fetchData}/>
+      <SearchButton onButtonClick={()=>setbtn(btn=>!btn)}/>
     </>
   );
 }
